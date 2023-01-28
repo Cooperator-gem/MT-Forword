656efec18a62ea0f810ef8ec0d00161d8b8baf0b
Akhil chandran <97656070+Cooperator-gem@users.noreply.github.com>
2023-01-28 07:54:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = int(os.environ.get("API_ID", 2819362))
+    API_HASH = os.environ.get("API_HASH", "578ce3d09fadd539544a327c45b55ee4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5612759440:AAGsHWbTEv2uffQcwkaAFoTdf-FDeHXR5Aw") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     CAPTION = os.environ.get("CAPTION", "")
-    FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
+    FROM_CHANNEL = os.environ.get("FROM_CHANNEL", "@moneiac_course")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
-    OWNER_ID = os.environ.get("OWNER_ID", 12345)
+    OWNER_ID = os.environ.get("OWNER_ID", "1211202733")
     LIMIT = int(os.environ.get("LIMIT", "25000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
     SESSION = os.environ.get("SESSION")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 12345))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", -1001601169374))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
