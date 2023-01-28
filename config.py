de9cdfe5dd66ca4c9c413e95cb908fd7ab4547e7
Akhil chandran <97656070+Cooperator-gem@users.noreply.github.com>
2023-01-28 08:01:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+I#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Dark Angel
 
@@ -17,7 +17,7 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", "1211202733")
     LIMIT = int(os.environ.get("LIMIT", "25000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION")
+    SESSION = os.environ.get("SESSION", "BQCfla0mR5gMIYbIrRR9Of6BaiAs2bROZPhw-oPiGtpFWc2ew-qwJnhlLzAKj75jf9AXkUklCkDQwRh-eMgAvFpOwVY47OqLiBO5mrOx-lgIr-2AFfcOu65VYTeAUa4YAGLOn320Y6dpaSQ4KADxqqGiqqwl2oTnsS28LQ8Tse0akZ57wkekm97vW93IJO6JFU28hrr7DQgG65MepL2Qv3wtfOq6-J3WPGsief8wusklWhRg7Lf7xkoqfG5ucYtTz2wXIZ9V0mOwfNYTbo-Ps58LjCv8yo7l65-i5vZqGKEVGjjMBjgEUx2e17oJ_NorLbUyVQB-Yj2ZEzklQLqZ4NcZSDF8rQA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", -1001601169374))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
